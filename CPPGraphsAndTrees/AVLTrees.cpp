c9b8612a88a60c48baf9a0f28c363d097d7f8bf4
Ayaanle11 <ayaanlemohamed540@gmail.com>
2022-07-20 17:25:29 +0300

    commiting

--- CPPGraphsAndTrees/AVLTrees.cpp
+++ CPPGraphsAndTrees/AVLTrees.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <list>
+#include <map>
 
+AVLTree::AVLTree() {
+
+}
+AVLNode* AVLTree::rightleft_rotation(BSTNode* rl)
+{
+
+}
+AVLNode* leftleft_rotation(BSTNode* llrotation) {
+
+}
+AVLNode* leftright_rotation(BSTNode*)
+{
+
+}
+AVLNode* rightright_rotation(BSTNode* rr_rotation)
+{
+
+}
+void display(AVLNode* node) {
+
+}
+void preorder(AVLNode* node) {
+
+}
+void inorder(AVLNode* node)
+{
+
+}
+void postorder(AVLNode* node) {
+
+}
--- CPPGraphsAndTrees/Graphs.hpp
+++ CPPGraphsAndTrees/Graphs.hpp
@@ -4,14 +4,20 @@
 #define GRAPHS_H
 #endif
 template <class T>
+struct adjListNode {
+	int value;
+	adjNode* next;
+};
+struct graphEdge {
+	int startV, endV, weight;
+};
+struct adjList {
+	struct adjListNode* node;
+};
 class Graphs {
 public:
-	Graphs();
-	void insert();
-	void delete();
-	int search(T data);
-	void moveLeft(T&& data);
-	void moveLeft(T&& data);
+	Graph();
+
 protected:
 	int left;
 	int rigth;
